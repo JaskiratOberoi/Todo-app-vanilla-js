a595bc2413b804abaa2e8954e82ebce8dfb63ef0
Jaskirat Singh Oberoi <Jso101196@gmail.com>
2020-04-12 01:20:26 +0530

    added completed elements

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
-document.addEventListener("DOMContentLoaded", getTodos);
+document.addEventListener("DOMContentLoaded", getAllTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("click", filterTodo);
@@ -56,7 +56,11 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     //Animation
     todo.classList.add("fall");
-    removeLocalTodos(todo);
+    if (todo.classList.contains("completed")) {
+      removeCompletedLocalStorage(todo.innerText);
+    } else {
+      removeLocalTodos(todo.innerText);
+    }
     todo.addEventListener("transitionend", function () {
       todo.remove();
     });
@@ -67,6 +71,11 @@ function deleteCheck(e) {
   if (item.classList[0] === "complete-btn") {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
+    if (todo.classList.contains("completed")) {
+      markCompletedLocalStorage(todo.innerText);
+    } else {
+      toggleCompletedLocalTodos(todo.innerText);
+    }
   }
 }
 
@@ -155,4 +164,88 @@ function removeLocalTodos(todo) {
   const todoIndex = Array.from(todoList.childNodes).indexOf(todo);
   todos.splice(todoIndex, 1);
   localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function markCompletedLocalStorage(todo) {
+  let completedTodos, todos;
+  if (localStorage.getItem("completedTodos") === null) {
+    completedTodos = [];
+  } else {
+    completedTodos = JSON.parse(localStorage.getItem("completedTodos"));
+  }
+
+  todos = JSON.parse(localStorage.getItem("todos"));
+
+  completedTodos.push(todo);
+  localStorage.setItem("completedTodos", JSON.stringify(completedTodos));
+
+  const todoIndex = Array.from(todoList.childNodes).indexOf(todo);
+  todos.splice(todoIndex, 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getCompletedTodos() {
+  let completedTodos;
+  if (localStorage.getItem("completedTodos") === null) {
+    completedTodos = [];
+  } else {
+    completedTodos = JSON.parse(localStorage.getItem("completedTodos"));
+  }
+
+  completedTodos.forEach(function (todo) {
+    //   Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    todoDiv.classList.add("completed");
+
+    //   Create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todo;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //DELETE BUTTON
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class = "fas fa-trash"></i>';
+    deleteButton.classList.add("delete-btn");
+    todoDiv.appendChild(deleteButton);
+
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
+  });
+}
+
+function toggleCompletedLocalTodos(todo) {
+  let completedTodos, todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  completedTodos = JSON.parse(localStorage.getItem("completedTodos"));
+
+  todos.push(todo);
+  const todoIndex = Array.from(todoList.childNodes).indexOf(todo);
+  completedTodos.splice(todoIndex, 1);
+  localStorage.setItem("completedTodos", JSON.stringify(completedTodos));
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function removeCompletedLocalTodos(todo) {
+  let completedTodos = JSON.parse(localStorage.getItem("completedTodos"));
+
+  const todoIndex = Array.from(todoList.childNodes).indexOf(todo);
+  completedTodos.splice(todoIndex, 1);
+  localStorage.setItem("completedTodos", JSON.stringify(completedTodos));
+}
+
+function getAllTodos() {
+  getTodos();
+  getCompletedTodos();
 }
